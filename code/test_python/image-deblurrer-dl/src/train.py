9215076e9461435738cde4540e91a30b4ed69f01
Alejandro Garnung Menéndez <agarnung@cinsystems.es>
2025-02-08 22:05:34 +0100

    Commented posible data augmentation

--- code/test_python/image-deblurrer-dl/src/train.py
+++ code/test_python/image-deblurrer-dl/src/train.py
@@ -92,6 +92,10 @@ print("Test images: ", len(x_val))
 transform = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((224, 224)), # ResNet and VGG input size standard
+    # Data augmentation
+    # transforms.RandomHorizontalFlip(p=0.5), 
+    # transforms.RandomRotation(10),  
+    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.ToTensor()
 ])
 
